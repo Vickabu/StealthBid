34f23790ca6ddf2a6e4b56fd1943b4cd3381fffa
Vick Alexa <142795784+Vickabu@users.noreply.github.com>
2024-12-11 22:29:20 +0100

    Update delete.js

--- src/js/api/listings/delete.js
+++ src/js/api/listings/delete.js
@@ -23,25 +23,3 @@ export async function deleteListing(id) {
     return false;
   }
 }
-
-export async function onDeletePost(event) {
-  event.stopPropagation();
-  const deleteButton = event.target;
-  const listingId = deleteButton.dataset.listingId;
-
-  if (!listingId) {
-    console.error("Listing ID not found.");
-    return;
-  }
-
-  if (confirm("Are you sure you want to delete this listing?")) {
-    const success = await deleteListing(listingId);
-
-    if (success) {
-      alert("Listing deleted successfully.");
-      window.location.href = "/";
-    } else {
-      alert("Failed to delete the listing.");
-    }
-  }
-}
